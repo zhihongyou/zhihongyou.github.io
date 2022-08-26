3e3edaacde4b97d73bf54a6b644f1b55d4e10a50
zhihongyou <zhihongyou6@gmail.com>
2022-08-26 08:03:25 +0800

    update

---
+++ data/auto/publications.el
@@ -0,0 +1,15 @@
+(TeX-add-style-hook
+ "publications"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "you12_analy_light_scatt_by_capil"
+    "you12_charac_applic_two_dimen_light"
+    "you18_geomet_mechan_microd_growin_bacter_colon"
+    "lemma19_stat_prop_auto_flow_2d_acti_nema"
+    "you19_mono_to_multil_trans_growin_bacter_colon"
+    "you20_nonrec_as_gener_route_to_travel_states"
+    "you2019growth"
+    "Pokawanvit22_acti_nema_defe_comp_incom"
+    "adkins22_dynam_activ_liquid_inter"))
+ :bibtex)
+
---
+++ publication/you-20-nonrec-as-gener-route-to-travel-states/auto/cite.el
@@ -0,0 +1,7 @@
+(TeX-add-style-hook
+ "cite"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "you20_nonrec_as_gener_route_to_travel_states"))
+ :bibtex)
+
